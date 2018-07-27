ac442f38ecfcf6a342cf76a5da198911e830d95f
Morgany Vieira Mendes <morgany@mangustotech.com>
2018-07-27 17:13:19 -0300

    Initial commit api-gateway + todo-service

---
+++ todo-service/src/main/java/com/github/morganyvm/todo/TodoServiceApplication.java
@@ -0,0 +1,12 @@
+package com.github.morganyvm.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoServiceApplication.class, args);
+	}
+}
