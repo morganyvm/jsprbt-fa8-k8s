5f6f9ad5723d266857a04d1792b27111de73339f
Morgany Vieira Mendes <morgany@mangustotech.com>
2018-08-01 09:36:15 -0300

    Keep defaults

--- todo-service/src/main/java/com/github/morganyvm/todo/TodoRepository.java
+++ todo-service/src/main/java/com/github/morganyvm/todo/TodoRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "todos", path = "todos")
+@RepositoryRestResource
 public interface TodoRepository extends PagingAndSortingRepository<Todo, Long> {
 
 	List<Todo> findByValue(@Param("value") String name);
